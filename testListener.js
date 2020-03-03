df2142cd40cb461891012c7d432ec161cc79989e
jalagor <johnrossrogla@gmail.com>
2020-03-03 14:36:45 -0700

    removed comments

--- testListener.js
+++ testListener.js
@@ -6,13 +6,17 @@ const parser = new Readline()
 const myPort = new SerialPort('/dev/cu.usbmodem14101', {
     baudRate: 9600
 })
+
+
 myPort.pipe(parser)
 myPort.on('open', onOpen)
-// myPort.on('data', onData)
 
 function onOpen(){
     console.log('open connection')
+    
 }
+
+
 parser.on('data', (data)=> { postBPM(data) } )
 
 function postBPM(number){
